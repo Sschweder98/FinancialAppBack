f5a52bf5f8cedaa74399fbd088082c687bad76c8
Sschweder98 <106622191+Sschweder98@users.noreply.github.com>
2022-07-15 20:22:58 +0200

    Did very much

--- Program.cs
+++ Program.cs
@@ -17,7 +17,9 @@ namespace financial
         {
             Initialize();
             _csvManager.loadCSV("C:\\Users\\sschw\\Downloads\\kskwn_export.CSV");
-            _dataMapper.map_csvData_to_finance_data(_csvManager.ksk_csv, ref database);
+            _dataMapper.setCategories((database._finance_categories).ToList());
+            _dataMapper.map_csvData_to_database(_csvManager.ksk_csv, ref database);
+            _dataMapper.getAdditionalData(ref database);
         }
 
         private static void getEntries()
@@ -38,7 +40,12 @@ namespace financial
 
     public class dataMapper
     {
-        public void map_csvData_to_finance_data(List<csvManager.ksk_csv_row> csv_data, ref FinancialContext database)
+        categoryManager _categoryManager;
+
+        public void setCategories(List<finance_categories> categories){
+            _categoryManager = new categoryManager(categories);
+        }
+        public void map_csvData_to_database(List<csvManager.ksk_csv_row> csv_data, ref FinancialContext database)
         {
             foreach (csvManager.ksk_csv_row row in csv_data)
             {
@@ -46,10 +53,34 @@ namespace financial
                 Int32 Count = (from finance_data tmp in database._finance_data where tmp.uniqe_key == unqiue select tmp).Count();
                 if (Count == 0)
                 {
-                    //Create Entry
-
+                    createDatabaseEntry(row, ref database, unqiue);
                 }
             }
+            database.SaveChanges();
+        }
+
+        public void createDatabaseEntry(csvManager.ksk_csv_row row, ref FinancialContext database, String unqiue)
+        {
+            finance_data tmp_object = new finance_data();
+            tmp_object.id = 0;
+            tmp_object.uniqe_key = unqiue;
+            tmp_object.date = Convert.ToDateTime(row.Buchungstag);
+            tmp_object.description1 = row.Buchungstext;
+            tmp_object.description2 = row.Verwendungszweck;
+            tmp_object.category = _categoryManager.getCategoryByName("Sonstiges").category_name;
+            tmp_object.cost_fixed = false;
+            tmp_object.recipient1 = row.Beguenstigter_Zahlungspflichtiger;
+            tmp_object.recipient2 = row.Kontonummer_IBAN;
+            tmp_object.value = Convert.ToDouble(row.Betrag);
+            database._finance_data.Add(tmp_object);
+        }
+
+        public void getAdditionalData(ref FinancialContext database){
+                foreach(finance_data entry in database._finance_data){
+                    entry.category = _categoryManager.findCategory(entry).category_name;
+                    entry.cost_fixed = _categoryManager.isFixed(entry); 
+                }
+                database.SaveChanges();
         }
 
         public string GenerateKey(csvManager.ksk_csv_row row)
@@ -57,7 +88,14 @@ namespace financial
             //date
             String unique = row.Buchungstag.Replace(".", "");
             //name of recipient
-            unique += "_" + row.Beguenstigter_Zahlungspflichtiger.ToLower().Replace(" ", "_").Substring(0, 5);
+            if (row.Beguenstigter_Zahlungspflichtiger.Length > 5)
+            {
+                unique += "_" + row.Beguenstigter_Zahlungspflichtiger.ToLower().Replace(" ", "_").Substring(0, 5);
+            }
+            else
+            {
+                unique += "_" + "XXXXX";
+            }
             //iban of recipient
             if (row.Kontonummer_IBAN.Length >= 5)
             {
@@ -65,10 +103,10 @@ namespace financial
             }
             else
             {
-                unique += "XXXXX";
+                unique += "_" + "XXXXX";
             }
             //value
-            unique += "_" + row.Betrag.Replace("_", "").Replace(",", "X");
+            unique += "_" + row.Betrag.Replace("-", "").Replace(",", "X");
             return unique;
         }
     }
@@ -85,7 +123,6 @@ namespace financial
             {
                 analyse_line(reader.ReadLine());
             }
-
         }
 
         private void analyse_line(String line)
@@ -166,11 +203,131 @@ namespace financial
 
         public finance_categories findCategory(finance_data _Data)
         {
-            if (_Data.description1.ToLower().Contains("agip"))
+            //Einnahmen
+            if (_Data.value > 0.00){
+                if (_Data.description1.ToLower().Contains("gehalt")){
+                    return getCategoryByName("Gehalt");
+                }
+                else if (_Data.description2.Contains("miete")){
+                    return getCategoryByName("Mieteinnahmen");
+                }
+                else{
+                    return getCategoryByName("Sonstige Einnahmen");
+                }
+            }
+
+            //Umbuchung
+            if (_Data.description1.ToLower().Contains("uebertrag") || _Data.recipient1.ToLower().Contains("sebastian schweder")){
+                return getCategoryByName("Umbuchung");
+            }
+
+            //Bargeldauszahlung
+            if (_Data.description1.ToLower().Contains("bargeldauszahlung") || _Data.description1.ToLower().Contains("auszahl.")){
+                return getCategoryByName("Bargeldauszahlung");
+            }
+
+            //Steuern
+            if (_Data.recipient1.ToLower().Contains("bundeskasse") || _Data.recipient1.ToLower().Contains("stadt") || _Data.recipient1.ToLower().Contains("finanzamt") ||
+             _Data.recipient1.ToLower().Contains("fuer fk schorndorf")){
+                return getCategoryByName("Bargeldauszahlung");
+            }
+
+            //Bars
+            if (_Data.recipient1.ToLower().Contains("ssk") || _Data.recipient1.ToLower().Contains("skybar")){
+                return getCategoryByName("Bargeldauszahlung");
+            }
+
+            //Tankstellen
+            if (_Data.recipient1.ToLower().Contains("agip") || 
+            _Data.recipient1.ToLower().Contains("aral") ||
+            _Data.recipient1.ToLower().Contains("avia") ||
+            _Data.recipient1.ToLower().Contains("tankpoint") ||
+            _Data.recipient1.ToLower().Contains("ran ts") ||
+            _Data.recipient1.ToLower().Contains("shell"))
             {
                 return getCategoryByName("Tankstelle");
             }
-            return null;
+
+            //Lebensmittel-Einkauf
+            if (_Data.recipient1.ToLower().Contains("netto marken") ||
+            _Data.recipient1.ToLower().Contains("ecenter") ||
+            _Data.recipient1.ToLower().Contains("edeka") ||
+            _Data.recipient1.ToLower().Contains("kaufland") ||
+            _Data.recipient1.ToLower().Contains("conad") ||
+            _Data.recipient1.ToLower().Contains("tedi fil") ||
+            _Data.recipient1.ToLower().Contains("backerei maurer") ||
+            _Data.recipient1.ToLower().Contains("rewe")
+            ){
+                return getCategoryByName("Lebensmittel");
+            }
+
+            //Hausverwaltung
+            if (_Data.recipient1.ToLower().Contains("hausverwaltung")){
+                return getCategoryByName("Hausverwaltung");
+            }
+
+            //Kredite
+            if (_Data.recipient1.ToLower().Contains("santander") || _Data.description1.ToLower().Contains("einzug rate")){
+                return getCategoryByName("Kredit");
+            }
+
+            //Versicherung
+            if (_Data.recipient1.ToLower().Contains("wwk") || _Data.recipient1.ToLower().Contains("versicherung")){
+                return getCategoryByName("Versicherung");
+            }
+
+            //Abonnements
+            if (_Data.recipient1.ToLower().Contains("fitness") || _Data.recipient1.ToLower().Contains("strato ag") || _Data.recipient1.ToLower().Contains("Fitness")){
+                return getCategoryByName("Abonnements");
+            }
+
+            //OnlineShopping
+            if (_Data.recipient1.ToLower().Contains("paypal") || _Data.recipient1.ToLower().Contains("amazon payments") || _Data.recipient1.ToLower().Contains("Fitness") || 
+            _Data.recipient1.ToLower().Contains("amazon eu")){
+                return getCategoryByName("Onlineshopping");
+            }
+
+            //FastFood
+            if (_Data.recipient1.ToLower().Contains("mcdonalds") || _Data.recipient1.ToLower().Contains("bk-sued")){
+                return getCategoryByName("FastFood");
+            }
+
+            //Restaurant
+            if (_Data.recipient1.ToLower().Contains("schwabenalm") || _Data.recipient1.ToLower().Contains("dimitra") || _Data.recipient1.ToLower().Contains("ketch may beef") || 
+            _Data.recipient1.ToLower().Contains("kesselhaus")){
+                return getCategoryByName("Restaurant");
+            }
+
+            //Technik/Gaming
+            if (_Data.recipient1.ToLower().Contains("media markt") || _Data.recipient1.ToLower().Contains("saturn")){
+                return getCategoryByName("Technik/Gaming");
+            }
+
+            //Mobilfunk
+            if (_Data.recipient1.ToLower().Contains("telefonica")){
+                return getCategoryByName("Mobilfunk");
+            }
+
+            //Arztkosten
+            if (_Data.recipient1.ToLower().Contains("dr. ")){
+                return getCategoryByName("Arztkosten");
+            }
+
+            //Sonstiges
+            return getCategoryByName("Sonstiges");
+
+            
+        }
+
+        public Boolean isFixed(finance_data _Data)
+        {
+            if (_Data.category == "Kredit" || _Data.category == "Versicherung" || _Data.category == "Abonnements")
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
         }
 
 
@@ -218,7 +375,7 @@ namespace financial
         public string category { get; set; }
         public Boolean cost_fixed { get; set; }
         public string recipient1 { get; set; }
-        public int recipient2 { get; set; }
+        public string recipient2 { get; set; }
         public double value { get; set; }
     }
 
